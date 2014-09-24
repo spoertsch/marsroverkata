44afc3de04d85638e3af243b6477c97abb17236a
spoertsch <spoertsch@gmail.com>
2014-09-24 22:46:54 +0200

    Added test for non negative x starting point

--- src/test/scala/MarsRoverSpec.scala
+++ src/test/scala/MarsRoverSpec.scala
@@ -16,16 +16,22 @@ class MarsRoverSpec extends FlatSpec with Matchers {
   }
 
   it should "have a default grid" in new MarsRoverFixture {
-    marsRover.getGrid() shouldBe (100,100)
+    marsRover.getGrid() shouldBe (100, 100)
   }
 
   it should "have a default starting point" in new MarsRoverFixture {
-    marsRover.getStartingPoint() shouldBe (0,0)
+    marsRover.getStartingPoint() shouldBe (0, 0)
   }
 
   it should "have a non default starting point" in {
-    val marsRover = new MarsRover(Grid(), 1,1)
-    marsRover.getStartingPoint() shouldBe (1,1)
+    val marsRover = new MarsRover(Grid(), 1, 1)
+    marsRover.getStartingPoint() shouldBe (1, 1)
+  }
+
+  it should "not allow a negative x starting point" in {
+    a[IllegalArgumentException] should be thrownBy {
+      new MarsRover(Grid(), -1, 1)
+    }
   }
 
 }
