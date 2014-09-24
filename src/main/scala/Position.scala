d2396bb5bfc27d1da9b26b4795e1883a630297e8
spoertsch <spoertsch@gmail.com>
2014-09-24 23:49:31 +0200

    Added test for get position as tupel

--- src/main/scala/Position.scala
+++ src/main/scala/Position.scala
@@ -2,5 +2,6 @@
  * Created by jst on 24.09.14.
  */
 case class Position(x: Integer, y: Integer) {
+  def getAsTupel(): (Integer, Integer) = ???
 
 }
---
+++ src/test/scala/PositionSpec.scala
@@ -0,0 +1,13 @@
+import org.scalatest.{Matchers, FlatSpec}
+
+/**
+ * Test class for Position
+ *
+ * Created by jst on 24.09.14.
+ */
+class PositionSpec extends FlatSpec with Matchers {
+  "Position" should "return the position as tupel (x,y)" in {
+    val pos = Position(1, 2)
+    pos.getAsTupel() shouldBe (1, 2)
+  }
+}
