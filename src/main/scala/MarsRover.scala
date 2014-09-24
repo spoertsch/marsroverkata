697167811b8a2b75ac64612e24f2ddbb4138b94c
spoertsch <spoertsch@gmail.com>
2014-09-24 22:00:43 +0200

    Implemented check for grid row count

--- src/main/scala/MarsRover.scala
+++ src/main/scala/MarsRover.scala
@@ -4,6 +4,7 @@
 class MarsRover(val gridColumns: Integer = 100, val gridRows: Integer = 100) {
 
   require(gridColumns >= 0)
+  require(gridRows >= 0)
 
   def getGrid() : (Integer, Integer) = (gridColumns, gridRows)
 
